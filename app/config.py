e54f66f13afee563cfe0d3ffae6272d604adb02c
collins-kibet-ngenoh <collins.kibet.ngenoh@gmail.com>
2024-08-20 17:42:55 +0300

    add db

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'your_secret_key')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://art_gallery_db_user:DW5IQXH7YRhiMYUIE2479h7wX3mpNmp8@dpg-cr2ajubv2p9s738cr1ng-a.oregon-postgres.render.com/art_gallery_db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'your_jwt_secret_key')
 
